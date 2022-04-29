f72bf52344b9a36cf9e9fff7800fba298640d19a
vinicius.padilha <vinicius.padilha@euax.com.br>
2022-04-29 14:30:48 -0300

    Created new tests for User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
     enum kind: [ :knight, :wizard ]
-    validates :level, numericality: { greater_than: 0, less_than_or_equal_to: 99}
+
+    validates :level, numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 99 }
 
     def title
         "#{self.kind} #{self.nickname} ##{self.level}"
